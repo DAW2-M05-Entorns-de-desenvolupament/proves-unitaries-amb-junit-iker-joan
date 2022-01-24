fcfa6850293637f0cad37fddaccf09bf44b630f6
Ikerlahera <71877676+Ikerlahera@users.noreply.github.com>
2022-01-24 13:14:58 +0100

    Add files via upload

--- projectonuevo/src/main/java/programajoaniker.java
+++ projectonuevo/src/main/java/programajoaniker.java
@@ -1,3 +1,6 @@
+
+import java.util.Scanner;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -9,4 +12,16 @@
  */
 public class programajoaniker {
     
+    public static void main(String[] args){
+       Scanner reader = new Scanner(System.in);
+       int numero1 = 0;
+       int numero2 = 0;
+    System.out.println("Introduce el primer número:");      
+    numero1 = reader.nextInt();
+      
+    System.out.println("Introduce el segundo número:");
+    numero2 = reader.nextInt();
+        int resultado = numero1+numero2;
+    System.out.println("La suma es " + numero1 + " + " + numero2 + " = " + resultado);
+}
 }
