9b33c8e9c39d1bd8207eae6db0c49f7e3fb6400d
JARus <joan.rus.02@gmail.com>
2022-01-24 13:12:03 +0100

    Canvis Joan

--- projectonuevo/src/main/java/programajoaniker.java
+++ projectonuevo/src/main/java/programajoaniker.java
@@ -1,12 +1,23 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+import java.util.Scanner;
 
-/**
- *
- * @author alumne
- */
+
 public class programajoaniker {
     
+public static void main{
+}
+
+
+public static void perell{
+
+    Scanner keyb = new Scanner(System.in);
+        System.out.println("Introdueix un nombre: ");
+        int Num1 = keyb.nextInt();
+        if (Num1%2 == 0) {
+            System.out.println("És parell");
+        }
+        else {
+            System.out.println("És imparell");
+        }
+
+}
 }
